b3683f437cd0df74a66e905c8be58d26ba673014
misteryZZZ <hendriawany@gmail.com>
2021-07-02 23:05:56 +0700

    Whatsapp 1.1

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDiXVq4lQtTPtGZWg2j6rsWZD0FDVZuocI",
+    authDomain: "whatsapp-mern-81dd8.firebaseapp.com",
+    projectId: "whatsapp-mern-81dd8",
+    storageBucket: "whatsapp-mern-81dd8.appspot.com",
+    messagingSenderId: "387768653623",
+    appId: "1:387768653623:web:fe5826a1e3cb81e9feacb2"
+  };
